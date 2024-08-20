f91cf16258ea114f9b5a4ec0167f64ea55ee9380
Satorufrekinggojo <161453433+Satorufrekinggojo@users.noreply.github.com>
2024-08-21 03:02:15 +0600

    Update Jaja.js

--- Jaja.js
+++ Jaja.js
@@ -6,7 +6,7 @@ const baseApiUrl = async () => {
 module.exports = {
   config: {
     name: "dalle",
-    aliases: ["bing", "create", "imagine"],
+    aliases: ["bing", "create", ""],
     version: "1.0",
     author: "Dipto",
     countDown: 15,
