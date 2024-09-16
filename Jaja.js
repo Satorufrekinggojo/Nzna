246119e7f9eeef1de7ac51fbf9c213d04f41ac53
Satorufrekinggojo <161453433+Satorufrekinggojo@users.noreply.github.com>
2024-09-16 20:34:10 +0600

    Update Jaja.js

--- Jaja.js
+++ Jaja.js
@@ -1,37 +1,47 @@
 const axios = require('axios');
-const baseApiUrl = async () => {
-  const base = await axios.get(`https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`);
-  return base.data.api;
-}; 
 module.exports = {
   config: {
-    name: "dalle",
-    aliases: ["bing", "create", ""],
+    name: "prompt",
     version: "1.0",
-    author: "Dipto",
-    countDown: 15,
+    author: "rehat--",
+    countDown: 5,
     role: 0,
-    description: "Generate images by Unofficial Dalle3",
-    category: "download",
-    guide: { en: "{pn} prompt" }
-  }, 
-  onStart: async({ api, event, args }) => {
-    const prompt = (event.messageReply?.body.split("dalle")[1] || args.join(" ")).trim();
-    if (!prompt) return api.sendMessage("❌| Wrong Format. ✅ | Use: 17/18 years old boy/girl watching football match on TV with 'Dipto' and '69' written on the back of their dress, 4k", event.threadID, event.messageID);
+    guide: { en: "{pn} <prompt>" },
+    longDescription: {
+      en: "Get image generator prompt by replying image and by text"
+    },
+    category: "image"
+  },
+  onStart: async function ({ message, event, args, api }) {
     try {
-       //const cookies = "cookies here (_U value)";
-const cookies = ["14Ty4V9DcK1wDBP2u38czo5B7qCOfx7sLXkgcrFFSimlJ4inJFCyuO71VbedFOpxe686wtvRtdyNrzHVssrZN8a6383489p3lGx-1Ja5ehkdv_7zXR4dfAIigSKKj0SwIqqCq5hv7DIJ6Welcydx77MCMU56EHpxJlGwQ8-J0YAOwA2WtOI0oicoYU5lJBXVgh3pU_3PauFLbQBx3lkR91w"];
-const randomCookie = cookies[Math.floor(Math.random() * cookies.length)];
-      const wait = api.sendMessage("Wait koro baby 😽", event.threadID);
-      const response = await axios.get(`${await baseApiUrl()}/dalle?prompt=${prompt}&key=dipto008&cookies=${randomCookie}`);
-const imageUrls = response.data.imgUrls || [];
-      if (!imageUrls.length) return api.sendMessage("Empty response or no images generated.", event.threadID, event.messageID);
-      const images = await Promise.all(imageUrls.map(url => axios.get(url, { responseType: 'stream' }).then(res => res.data)));
-    api.unsendMessage(wait.messageID);
-   api.sendMessage({ body: `✅ | Here's Your Generated Photo 😘`, attachment: images }, event.threadID, event.messageID);
+      let imageUrl;
+      let khankirChele = args.join(" ");
+
+      if (event.type === "message_reply") {
+        if (["photo", "sticker"].includes(event.messageReply.attachments[0]?.type)) {
+          imageUrl = event.messageReply.attachments[0].url;
+        } else {
+          return api.sendMessage({ body: "Reply must be an image." }, event.threadID);
+        }
+      } else if (args[0]?.match(/(https?:\/\/.*\.(?:png|jpg|jpeg))/g)) {
+        imageUrl = args[0];
+      } else if (!khankirChele) {
+        return api.sendMessage({ body: "Reply to an image or provide a prompt." }, event.threadID);
+      }
+
+      if (imageUrl) {
+        const response = await axios.post(`https://rehatdesu.xyz/api/imagine/i2p?url=${encodeURIComponent(imageUrl)}&apikey=rehatdiag84`);
+        const description = response.data.result;
+
+        await message.reply(description);
+      } else if (khankirChele) {
+        const response = await axios.get(`https://rehatdesu.xyz/api/imagine/t2p?text=${encodeURIComponent(khankirChele)}&apikey=rehatdiag84`);
+        const prompt = response.data.response;
+        await message.reply(prompt);
+      }
     } catch (error) {
       console.error(error);
-      api.sendMessage(`Generation failed!\nError: ${error.message}`, event.threadID, event.messageID);
+      message.reply("An error occurred.");
     }
   }
-}
+};
